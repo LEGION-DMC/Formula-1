4e80a71d12f3a0d73bfe22fd8c7fa17d97247cf6
Ł₤ǤÎØ₦ <jonathanrand.zxz@gmail.com>
2025-09-06 11:56:46 +0800

    Update results.js

--- Data/results.js
+++ Data/results.js
@@ -1,26 +1,28 @@
+// Личный зачёт
 const driversStandings = [
-    { position: 1, name: "Оскар Пиастри", country: "au", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109", points: 309},
-    { position: 2, name: "Ландо Норрис", country: "gb", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109", points: 275},
-    { position: 3, name: "Шарль Леклер", country: "mc", team: "Ferrari", teamLogo: "Ferrari-m.png", teamColor: "#b80f0f", points: 151},
-    { position: 4, name: "Льюис Хэмильтон", country: "gb", team: "Ferrari", teamLogo: "Ferrari-m.png", teamColor: "#b80f0f", points: 109},
-    { position: 5, name: "Джордж Рассел", country: "gb", team: "Mercedes", teamLogo: "Mercedes-m.png", teamColor: "#7a7272", points: 184},
-    { position: 6, name: "Кими Антонелли", country: "it", team: "Mercedes", teamLogo: "Mercedes-m.png", teamColor: "#7a7272", points: 64},
-    { position: 7, name: "Макс Ферстаппен", country: "nl", team: "Red Bull", teamLogo: "RedBull-m.png", teamColor: "#313247", points: 205},
-    { position: 8, name: "Юки Цунода", country: "jp", team: "Red Bull", teamLogo: "RedBull-m.png", teamColor: "#313247", points: 12},
-    { position: 9, name: "Карлос Сайнс", country: "es", team: "Williams", teamLogo: "Williams-m.png", teamColor: "#7b74fc", points: 16},
-    { position: 10, name: "Александр Албон", country: "th", team: "Williams", teamLogo: "Williams-m.png", teamColor: "#7b74fc", points: 64},
-    { position: 11, name: "Нико Хюлькенберг", country: "de", team: "Sauber", teamLogo: "Sauber-m.png", teamColor: "#21ad17", points: 37},
-    { position: 12, name: "Габриэл Бортолето", country: "br", team: "Sauber", teamLogo: "Sauber-m.png", teamColor: "#21ad17", points: 14},
-    { position: 13, name: "Лиам Лоусон", country: "nz", team: "Racing Bulls", teamLogo: "RacingBulls-m.png", teamColor: "#ddebdd", points: 20},
-    { position: 14, name: "Исаак Хаджар", country: "fr", team: "Racing Bulls", teamLogo: "RacingBulls-m.png", teamColor: "#ddebdd", points: 37},
-    { position: 15, name: "Лэнс Стролл", country: "ca", team: "Aston Martin", teamLogo: "AstonMartin-m.png", teamColor: "#fbff00", points: 32},
-    { position: 16, name: "Фернандо Алонсо", country: "es", team: "Aston Martin", teamLogo: "AstonMartin-m.png", teamColor: "#fbff00", points: 30},
-    { position: 17, name: "Эстебан Окон", country: "fr", team: "Haas", teamLogo: "Haas-m.png", teamColor: "#cf1d1d", points: 28},
-    { position: 18, name: "Оливер Берман", country: "gb", team: "Haas", teamLogo: "Haas-m.png", teamColor: "#cf1d1d", points: 16},
-    { position: 19, name: "Пьер Гасли", country: "fr", team: "Alpine", teamLogo: "Alpine-m.png", teamColor: "#a1459c", points: 20},
-    { position: 20, name: "Франко Колапинто", country: "ar", team: "Alpine", teamLogo: "Alpine-m.png", teamColor: "#a1459c", points: 0},
+    { position: 1, name: "Оскар Пиастри", points: 309, country: "au", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109"},
+    { position: 2, name: "Ландо Норрис", points: 275, country: "gb", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109"},
+    { position: 3, name: "Шарль Леклер", points: 151, country: "mc", team: "Ferrari", teamLogo: "Ferrari-m.png", teamColor: "#b80f0f"},
+    { position: 4, name: "Льюис Хэмильтон", points: 109, country: "gb", team: "Ferrari", teamLogo: "Ferrari-m.png", teamColor: "#b80f0f"},
+    { position: 5, name: "Джордж Рассел", points: 184, country: "gb", team: "Mercedes", teamLogo: "Mercedes-m.png", teamColor: "#7a7272"},
+    { position: 6, name: "Кими Антонелли", points: 64, country: "it", team: "Mercedes", teamLogo: "Mercedes-m.png", teamColor: "#7a7272"},
+    { position: 7, name: "Макс Ферстаппен", points: 205, country: "nl", team: "Red Bull", teamLogo: "RedBull-m.png", teamColor: "#313247"},
+    { position: 8, name: "Юки Цунода", points: 12, country: "jp", team: "Red Bull", teamLogo: "RedBull-m.png", teamColor: "#313247"},
+    { position: 9, name: "Карлос Сайнс", points: 16, country: "es", team: "Williams", teamLogo: "Williams-m.png", teamColor: "#7b74fc"},
+    { position: 10, name: "Александр Албон", points: 64, country: "th", team: "Williams", teamLogo: "Williams-m.png", teamColor: "#7b74fc"},
+    { position: 11, name: "Нико Хюлькенберг", points: 37, country: "de", team: "Sauber", teamLogo: "Sauber-m.png", teamColor: "#21ad17"},
+    { position: 12, name: "Габриэл Бортолето", points: 14, country: "br", team: "Sauber", teamLogo: "Sauber-m.png", teamColor: "#21ad17"},
+    { position: 13, name: "Лиам Лоусон", points: 20, country: "nz", team: "Racing Bulls", teamLogo: "RacingBulls-m.png", teamColor: "#ddebdd"},
+    { position: 14, name: "Исаак Хаджар", points: 37, country: "fr", team: "Racing Bulls", teamLogo: "RacingBulls-m.png", teamColor: "#ddebdd"},
+    { position: 15, name: "Лэнс Стролл", points: 32, country: "ca", team: "Aston Martin", teamLogo: "AstonMartin-m.png", teamColor: "#fbff00"},
+    { position: 16, name: "Фернандо Алонсо", points: 30, country: "es", team: "Aston Martin", teamLogo: "AstonMartin-m.png", teamColor: "#fbff00"},
+    { position: 17, name: "Эстебан Окон", points: 28, country: "fr", team: "Haas", teamLogo: "Haas-m.png", teamColor: "#cf1d1d"},
+    { position: 18, name: "Оливер Берман", points: 16, country: "gb", team: "Haas", teamLogo: "Haas-m.png", teamColor: "#cf1d1d"},
+    { position: 19, name: "Пьер Гасли", points: 20, country: "fr", team: "Alpine", teamLogo: "Alpine-m.png", teamColor: "#a1459c"},
+    { position: 20, name: "Франко Колапинто", points: 0, country: "ar", team: "Alpine", teamLogo: "Alpine-m.png", teamColor: "#a1459c"},
 ];
 
+// Кубок конструкторов
 const constructorsStandings = [
     { position: 1, team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109"},
     { position: 2, team: "Ferrari", teamLogo: "Ferrari-m.png", teamColor: "#b80f0f"},
@@ -34,6 +36,7 @@ const constructorsStandings = [
     { position: 10, team: "Alpine", teamLogo: "Alpine-m.png", teamColor: "#a1459c"},
 ];
 
+// Спринт
 const sprintStandings = [
     { position: 1, name: "Оскар Пиастри", country: "au", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109", points: 21},
     { position: 2, name: "Ландо Норрис", country: "gb", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109", points: 15},
@@ -57,6 +60,7 @@ const sprintStandings = [
     { position: 20, name: "Франко Колапинто", country: "ar", team: "Alpine", teamLogo: "Alpine-m.png", teamColor: "#a1459c", points: 0},
 ];
 
+// Победители 
 const raceWinners = [
     { grandPrix: "Китай Бахрейн Саудовская Аравия Майами Испания Бельгия Нидерланды", position: "1", winner: "Оскар Пиастри", country: "au", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109"},
     { grandPrix: "Австралия Манако Австрия Великобритания Венгрия", position: "2", winner: "Ландо Норрис", country: "gb", team: "McLaren", teamLogo: "McLaren-m.png", teamColor: "#e07109"},
@@ -64,44 +68,46 @@ const raceWinners = [
     { grandPrix: "Канада", position: "4", winner: "Джордж Рассел", country: "gb", team: "Mercedes", teamLogo: "Mercedes-m.png", teamColor: "#7a7272"},
 ];
 
+// Расчёт очков команды
 function calculateTeamPoints() {
-  const teamPoints = {};
-  const teamAdditionalPoints = {};
+    const teamPoints = {};
+    const teamAdditionalPoints = {};
 
-  // Собираем дополнительные очки для команд
-  constructorsStandings.forEach(team => {
-    if (team.points) {
-      teamAdditionalPoints[team.team] = team.points;
-    }
-  });
+    // Доп. очки команд
+    constructorsStandings.forEach(team => {
+        if (team.points) {
+            teamAdditionalPoints[team.team] = team.points;
+        }
+    });
 
-  // Суммируем очки всех пилотов команды
-  driversStandings.forEach(driver => {
-    if (!teamPoints[driver.team]) {
-      teamPoints[driver.team] = 0;
-    }
-    teamPoints[driver.team] += driver.points;
-  });
+    // Суммирование очков пилотов команды
+    driversStandings.forEach(driver => {
+        if (!teamPoints[driver.team]) {
+            teamPoints[driver.team] = 0;
+        }
+        teamPoints[driver.team] += driver.points;
+    });
 
-  // Обновляем массив constructorsStandings
-  constructorsStandings.forEach(team => {
-    const basePoints = teamPoints[team.team] || 0;
-    const additionalPoints = teamAdditionalPoints[team.team] || 0;
-    team.points = basePoints + additionalPoints;
-  });
+    // Обновление массива constructorsStandings
+    constructorsStandings.forEach(team => {
+        const basePoints = teamPoints[team.team] || 0;
+        const additionalPoints = teamAdditionalPoints[team.team] || 0;
+        team.points = basePoints + additionalPoints;
+    });
 
-  // Сортируем команды по очкам
-  constructorsStandings.sort((a, b) => b.points - a.points);
-  
-  // Обновляем позиции
-  constructorsStandings.forEach((team, index) => {
-    team.position = index + 1;
-  });
+    // Сортировка команды по очкам
+    constructorsStandings.sort((a, b) => b.points - a.points);
+    
+    // Обновление позиций
+    constructorsStandings.forEach((team, index) => {
+        team.position = index + 1;
+    });
 }
 
-// Вызываем расчёт очков при загрузке
+// Расчёт очков
 calculateTeamPoints();
 
+// Рендеринг страницы
 function renderResults() {
     const content = document.getElementById('content');
     content.innerHTML = `
@@ -110,15 +116,16 @@ function renderResults() {
                 <div class="drivers-standings" id="driversStandings"></div>
                 <div class="constructors-standings" id="constructorsStandings"></div>
                 <div class="sprint-standings" id="sprintStandings"></div>
-				<div class="race-winners" id="raceWinners"></div>
+                <div class="race-winners" id="raceWinners"></div>
             </div>
         </div>
     `;
 
+    // Рендер таблиц
     renderDriversStandings();
     renderConstructorsStandings();
     renderSprintStandings();
-	renderRaceWinners();
+    renderRaceWinners();
 
     // Выделение пилотов при выборе команды
     document.querySelectorAll('.constructor-row').forEach(row => {
@@ -129,15 +136,18 @@ function renderResults() {
     });
 }
 
+// Рендеринг таблицы личного зачёта пилотов
 function renderDriversStandings() {
     const container = document.getElementById('driversStandings');
+    
+    // Сортировка по очкам
     const sortedDrivers = [...driversStandings]
         .sort((a, b) => b.points - a.points)
         .map((driver, index) => ({ ...driver, position: index + 1 }));
     
     let html = `<h2>Личный зачёт</h2><div class="drivers-columns">`;
     
-    // Разделение на 2 колонки
+    // Разделение на 2 колонки по 10 пилотов
     for (let i = 0; i < 2; i++) {
         html += `<div class="drivers-column">`;
         sortedDrivers.slice(i * 10, (i + 1) * 10).forEach(driver => {
@@ -160,7 +170,7 @@ function renderDriversStandings() {
     html += `</div>`;
     container.innerHTML = html;
 
-    // Анимация при наведении
+    // Aнимация при наведении на строки
     document.querySelectorAll('.driver-row').forEach(row => {
         row.addEventListener('mouseenter', () => {
             row.style.transform = 'translateX(10px)';
@@ -171,14 +181,18 @@ function renderDriversStandings() {
     });
 }
 
+// Рендеринга таблицы кубка конструкторов
 function renderConstructorsStandings() {
     const container = document.getElementById('constructorsStandings');
+    
+    // Сортировка по очкам
     const sortedTeams = [...constructorsStandings]
         .sort((a, b) => b.points - a.points)
         .map((team, index) => ({ ...team, position: index + 1 }));
     
     let html = `<h2>Кубок конструкторов</h2><div class="constructors-list">`;
     
+    // Команды
     sortedTeams.slice(0, 10).forEach(team => {
         html += `
             <div class="constructor-row" data-team="${team.team}" data-team-color="${team.teamColor}" style="border-left-color: ${team.teamColor}">
@@ -195,7 +209,7 @@ function renderConstructorsStandings() {
     html += `</div>`;
     container.innerHTML = html;
 
-    // Анимация при наведении
+    // Анимация при наведении на строки
     document.querySelectorAll('.constructor-row').forEach(row => {
         row.addEventListener('mouseenter', () => {
             row.style.transform = 'translateX(10px)';
@@ -206,15 +220,18 @@ function renderConstructorsStandings() {
     });
 }
 
+// Рендеринг таблицы спринтерских гонок
 function renderSprintStandings() {
     const container = document.getElementById('sprintStandings');
+    
+    // Ортировка по очкам
     const sortedSprint = [...sprintStandings]
         .sort((a, b) => b.points - a.points)
         .map((driver, index) => ({ ...driver, position: index + 1 }));
     
     let html = `<h2>Спринтерская гонка</h2><div class="sprint-columns">`;
     
-    // Разделение на 2 колонки
+    // Разделение на 2 колонки по 10 пилотов
     for (let i = 0; i < 2; i++) {
         html += `<div class="sprint-column">`;
         sortedSprint.slice(i * 10, (i + 1) * 10).forEach(driver => {
@@ -237,7 +254,7 @@ function renderSprintStandings() {
     html += `</div>`;
     container.innerHTML = html;
 
-    // Анимация при наведении
+    // Aнимация при наведении на строки
     document.querySelectorAll('.sprint-row').forEach(row => {
         row.addEventListener('mouseenter', () => {
             row.style.transform = 'translateX(10px)';
@@ -248,20 +265,22 @@ function renderSprintStandings() {
     });
 }
 
+// Рендеринг таблицы победителей гран-при
 function renderRaceWinners() {
     const container = document.getElementById('raceWinners');
     let html = `<h2>Чемпионы этапов</h2><div class="winners-list">`;
     
+    // Победители гран-при
     raceWinners.forEach(race => {
         html += `
             <div class="winner-row" data-team="${race.team}">
                 <div class="winner-info">
-				    <span class="position">${race.position}</span>
-					<img src="Images/Flags/${race.country}.svg" alt="${race.country}" class="flag">
+                    <span class="position">${race.position}</span>
+                    <img src="Images/Flags/${race.country}.svg" alt="${race.country}" class="flag">
                     <span class="winner-name">${race.winner}</span>
                     <img src="Images/Teams/${race.teamLogo}" alt="${race.team}" class="team-logo">
                 </div>
-				<span class="grand-prix">${race.grandPrix}</span>
+                <span class="grand-prix">${race.grandPrix}</span>
             </div>
         `;
     });
@@ -269,7 +288,7 @@ function renderRaceWinners() {
     html += `</div>`;
     container.innerHTML = html;
 
-    // Анимация при наведении
+    // Анимацию при наведении на строки
     document.querySelectorAll('.winner-row').forEach(row => {
         row.addEventListener('mouseenter', () => {
             row.style.transform = 'translateX(10px)';
@@ -280,6 +299,7 @@ function renderRaceWinners() {
     });
 }
 
+// Выделение пилотов определённой команды
 function highlightTeamDrivers(team) {
     // Сброс предыдущего выделения
     document.querySelectorAll('.driver-row, .sprint-row').forEach(row => {
@@ -287,7 +307,7 @@ function highlightTeamDrivers(team) {
         row.style.borderLeftColor = 'transparent';
     });
 
-    // Выделение текущей команды
+    // Выделение команды
     document.querySelectorAll(`[data-team="${team}"]`).forEach(row => {
         const teamColor = row.getAttribute('data-team-color');
         row.style.opacity = '1';
@@ -295,19 +315,7 @@ function highlightTeamDrivers(team) {
     });
 }
 
+// Инициализация при загрузке вкладки
 if (window.location.hash === '#results') {
     renderResults();
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
