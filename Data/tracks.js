39bf393dce11301ae7eda17748217045442eace8
Ł₤ǤÎØ₦ <jonathanrand.zxz@gmail.com>
2026-09-12 22:19:38 +0800

    Q1

--- Data/tracks.js
+++ Data/tracks.js
@@ -192,7 +192,7 @@ const tracksData = [
         direction: "по часовой стрелке",
         firstrace: "2026",
         lapRecord: "---",
-        qulRecord: "P3 - 1:32.797, К. Антонелли - Mercedes - 2026",
+        qulRecord: "1:32.797, К. Антонелли - Mercedes - 2026",
     },
     {   trackName: "Городская трасса Баку",
         id: "baku",
