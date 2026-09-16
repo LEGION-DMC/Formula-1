a898596df8a9e3d13b42dd87b1286d77618789a5
Ł₤ǤÎØ₦ <jonathanrand.zxz@gmail.com>
2026-09-16 15:14:23 +0800

    Update main.js

--- Data/main.js
+++ Data/main.js
@@ -50,7 +50,7 @@ function mapWttrWeatherType(description) {
 
 async function fetchWeatherWttr(location) {
     try {
-        const url = `http://wttr.in/${encodeURIComponent(location).replace(/%2g, ',')}?format=j1`;
+        const url = `http://wttr.in/${encodeURIComponent(location)}?format=j1`;
         const response = await fetch(url);
         
         if (!response.ok) throw new Error('Ошибка запроса');
